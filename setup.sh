a93ffede6731a8b80ca2a97b874cb1f6b61baab3
Abhishek Rana <abhishekrana502@gmail.com>
2017-07-16 02:14:07 +0200

    setup.sh Modified

--- setup.sh
+++ setup.sh
@@ -2,4 +2,11 @@
 
 # Vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp vim/.vimrc ~
+wget https://raw.githubusercontent.com/abhishekrana/dotfiles/master/.vimrc -O ~/.vimrc
+vim +BundleInstall +qall
+
+# Tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+wget https://raw.githubusercontent.com/abhishekrana/dotfiles/master/.tmux.conf -O ~/.tmux.conf
+<leader_key> I
+
