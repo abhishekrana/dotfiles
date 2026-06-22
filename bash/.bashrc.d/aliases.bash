1ab1c099d455aaa0d7bfe3a6b8ffb3639f848a57
Abhishek Rana <abhishekrana502@gmail.com>
2026-06-22 13:15:02 +0200

    gbr: bump to last 500 branches

--- bash/.bashrc.d/aliases.bash
+++ bash/.bashrc.d/aliases.bash
@@ -18,8 +18,8 @@ alias gp='git push'
 alias gs='git status'
 # alias gst='git status'
 alias gb='git branch --sort=committerdate --format="%(refname:short) %(committerdate:relative)" | tail -20 | awk -F" " "{name=\$1; \$1=\"\"; printf \"%-50s (%s)\\n\", name, substr(\$0,2)}" && echo "" && echo "* $(git branch --show-current)"'
-# gbr: remote branches (last 100 by recency) with tip author; run gf first to refresh
-alias gbr='git branch -r --sort=committerdate --format="%(refname:short)%09%(authorname)%09%(committerdate:relative)" | grep -v "/HEAD" | tail -100 | awk -F"\t" "{ printf \"%-60s %-20s (%s)\\n\", \$1, \$2, \$3 }"'
+# gbr: remote branches (last 500 by recency) with tip author; run gf first to refresh
+alias gbr='git branch -r --sort=committerdate --format="%(refname:short)%09%(authorname)%09%(committerdate:relative)" | grep -v "/HEAD" | tail -500 | awk -F"\t" "{ printf \"%-60s %-20s (%s)\\n\", \$1, \$2, \$3 }"'
 alias gdm='hunk diff origin/main...HEAD'
 alias gw='while clear; do git diff --stat --color && echo "---" && git diff --color | head -60; sleep 2; done'
 
